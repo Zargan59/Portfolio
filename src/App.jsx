e5e6d5c552aba0b7ddfbb1064ddb79c93ca6678e
Tristan Destoop <tristan.Destoop@gmail.com>
2023-06-15 09:08:29 +0200

    Modification de l'adresse

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/Portfolio' element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
