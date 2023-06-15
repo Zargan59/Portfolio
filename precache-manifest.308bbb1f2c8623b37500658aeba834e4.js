af9d6269892bf1a7c9edb14afe7cdc284ab1a4e1
Tristan Destoop <tristan.Destoop@gmail.com>
2023-06-15 08:46:08 +0200

    Updates

---
+++ precache-manifest.308bbb1f2c8623b37500658aeba834e4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "03566303fdffa5e4e1c0",
+    "url": "/Portfolio/static/css/main.4c6292b8.chunk.css"
+  },
+  {
+    "revision": "03566303fdffa5e4e1c0",
+    "url": "/Portfolio/static/js/main.03566303.chunk.js"
+  },
+  {
+    "revision": "f495edba7c96d0975432",
+    "url": "/Portfolio/static/js/1.f495edba.chunk.js"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "636a46bc7ad7cdf1d357faf9f31994e3",
+    "url": "/Portfolio/static/media/github_PNG40.636a46bc.png"
+  },
+  {
+    "revision": "6fd4aac96dbcf4a2540159964a180b14",
+    "url": "/Portfolio/static/media/Linkedin.6fd4aac9.png"
+  },
+  {
+    "revision": "f76b8b796b56095975be25f871af820f",
+    "url": "/Portfolio/static/media/Desktop - 1.f76b8b79.png"
+  },
+  {
+    "revision": "adb5b0c57d354de3742a95e93a59d926",
+    "url": "/Portfolio/static/media/Accueil.adb5b0c5.png"
+  },
+  {
+    "revision": "9620305847f024df7a1e320b4c629e16",
+    "url": "/Portfolio/static/media/Kanap.96203058.png"
+  },
+  {
+    "revision": "c31ac795d358e8384632eaffc47ae9b7",
+    "url": "/Portfolio/index.html"
+  }
+];
